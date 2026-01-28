da4677136e1b792cc07c826d042db583a96b8823
Awasthi95 <shouryaawasthi45@gmail.com>
2026-01-28 15:29:49 +0530

    asynch

--- day1/promises/index.js
+++ day1/promises/index.js
@@ -1,11 +1,33 @@
-const myPromise = new Promise((resolve, reject) => {
-    let marks = 80;
-    if(marks>30){
-        resolve("pass");
-    }else{
-        reject("fail");
-    }}
-)
-myPromise.then((msg) =>console.log(msg))
-      .catch((error) => console.log("error occured."))
+const getData = async () =>
+{
+    const response = await fetch('https://fakestoreapi.com/products');
+    const res =await response.json()
+    console.log(res);
+
+}
+getData();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const myPromise = new Promise((resolve, reject) => {
+  //  let marks = 80;
+    //if(marks>30){
+      //  resolve("pass");
+    //}else{
+      //  reject("fail");
+    //}}
+//)
+//myPromise.then((msg) =>console.log(msg))
+  //    .catch((error) => console.log("error occured."))
       // console.log(myPromise);
